7f831c17c943900ca556f5fec0987f996a1e9955
Florian SIMUNEK <florian.simunek@gmail.com>
2023-01-30 12:41:24 +0100

    :fire::fire::fire: Remove code

--- scripts/app.js
+++ scripts/app.js
@@ -15,45 +15,6 @@ class App {
 
 		this.initSearchBar();
 		this.initFiltersInputs();
-
-		const ingredientsOptions = document.querySelector(".filters__ingredients__option");
-		const ingredientsArrow = document.querySelector(".filters__ingredients .arrow__block");
-
-		ingredientsArrow.addEventListener("click", () => {
-			if (ingredientsOptions.style.display === "none") {
-				ingredientsOptions.style.display = "flex";
-				ingredientsArrow.classList.add("open");
-			} else {
-				ingredientsOptions.style.display = "none";
-				ingredientsArrow.classList.remove("open");
-			}
-		});
-
-		const devicesOptions = document.querySelector(".filters__devices__option");
-		const devicesArrow = document.querySelector(".filters__devices .arrow__block");
-
-		devicesArrow.addEventListener("click", () => {
-			if (devicesOptions.style.display === "none") {
-				devicesOptions.style.display = "flex";
-				devicesArrow.classList.add("open");
-			} else {
-				devicesOptions.style.display = "none";
-				devicesArrow.classList.remove("open");
-			}
-		});
-
-		const utensilsOptions = document.querySelector(".filters__utensils__option");
-		const utensilsArrow = document.querySelector(".filters__utensils .arrow__block");
-
-		utensilsArrow.addEventListener("click", () => {
-			if (utensilsOptions.style.display === "none") {
-				utensilsOptions.style.display = "flex";
-				utensilsArrow.classList.add("open");
-			} else {
-				utensilsOptions.style.display = "none";
-				utensilsArrow.classList.remove("open");
-			}
-		});
 	}
 
 	initSearchBar() {
@@ -62,7 +23,6 @@ class App {
 			const MIN_INPUT_LENGTH = 3;
 
 			this._Recipes.forEach((recipe) => {
-				//TODO: without "à é"
 				const FILTER_NAME = recipe.name.toLowerCase().trim().includes(inputValue);
 				const FILTER_DESCRIPTION = recipe.description.toLowerCase().trim().includes(inputValue);
 				const FILTER_INGREDIENTS = recipe.ingredients.some((item) => item.ingredient.toLowerCase().trim().includes(inputValue));
@@ -80,50 +40,7 @@ class App {
 		});
 	}
 
-	initFiltersInputs() {
-		const ingredientsOption = document.querySelector(".filters__ingredients__option");
-		const devicesOption = document.querySelector(".filters__devices__option");
-		const utensilsOption = document.querySelector(".filters__utensils__option");
-
-		this._filtersInputs.forEach((input) => {
-			let defaultInputValue = input.value;
-
-			input.addEventListener("focus", () => {
-				input.value = "";
-				input.style.width = "65vw";
-			});
-
-			input.addEventListener("blur", () => {
-				input.value = defaultInputValue;
-				input.style.width = "170px";
-			});
-		});
-
-		let ingredientsList = this.getIngredientsList();
-		let devicesList = this.getDevicesList();
-		let utensilsList = this.getUtensilsList();
-
-		ingredientsList.forEach((ingredient) => {
-			let li = document.createElement("li");
-			li.textContent = ingredient;
-
-			ingredientsOption.append(li);
-		});
-
-		devicesList.forEach((device) => {
-			let li = document.createElement("li");
-			li.textContent = device;
-
-			devicesOption.append(li);
-		});
-
-		utensilsList.forEach((utensil) => {
-			let li = document.createElement("li");
-			li.textContent = utensil;
-
-			utensilsOption.append(li);
-		});
-	}
+	initFiltersInputs() {}
 
 	getIngredientsList() {
 		let list = [];
