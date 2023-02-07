420b99e2b8475411dcdb0acaa9d56d969157b644
Florian SIMUNEK <florian.simunek@gmail.com>
2023-02-07 22:13:39 +0100

    :sparkles: Add close option function

--- scripts/app.js
+++ scripts/app.js
@@ -3,7 +3,7 @@ class App {
 		this._data = recipes;
 		this._Recipes = [];
 		this._optionsLists = { ingredients: [], appliance: [], ustensils: [] };
-		this._selectedOptions = { ingredients: [], appliance: [], ustensils: [] };
+		this._selectedOptionsLists = { ingredients: [], appliance: [], ustensils: [] };
 		this._optionsListsElements = {
 			ingredients: document.querySelector(".filters__option__ingredients__list"),
 			appliance: document.querySelector(".filters__option__devices__list"),
@@ -23,11 +23,14 @@ class App {
 			this._Recipes[i].createCardDOM();
 		});
 
-		this.initOptions();
+		this.initOptionsLists();
+		this.appendOptionsToLists();
+		this.initEventsToOptions();
 		this.initSearchBar();
+		this.initFiltersInputs();
 	}
 
-	initOptions() {
+	initOptionsLists() {
 		this._Recipes.forEach((recipe) => {
 			recipe.ingredients.forEach((item) => {
 				this._optionsLists.ingredients.push({ name: clear(item.ingredient), selected: false, disabled: false });
@@ -45,8 +48,135 @@ class App {
 		for (let type in this._optionsLists) {
 			this._optionsLists[type] = [...new Map(this._optionsLists[type].map((item) => [item.name, item])).values()];
 		}
+	}
 
-		this.initFiltersInputs();
+	appendOptionsToLists() {
+		for (let type in this._optionsListsElements) {
+			this._optionsLists[type].forEach((option) => {
+				let li = createBlock("li", [
+					{ name: "class", value: "filters__option__list__item" },
+					{ name: "data-value", value: option.name },
+					{ name: "data-type", value: type },
+				]);
+				li.textContent = option.name;
+
+				this._optionsListsElements[type].append(li);
+			});
+		}
+	}
+
+	initEventsToOptions() {
+		this._optionsElements = document.querySelectorAll(".filters__option__list__item");
+
+		this._optionsElements.forEach((option) => {
+			option.addEventListener("click", () => {
+				let clickedItem = this._optionsLists[option.dataset.type].filter((item) => clear(item.name) === clear(option.dataset.value));
+				this._optionsLists[option.dataset.type].map(() => {
+					clickedItem[0].selected = true;
+					clickedItem[0].disabled = true;
+				});
+
+				this.refreshOptionsDisplay();
+			});
+		});
+	}
+
+	refreshOptionsDisplay() {
+		for (let type in this._optionsLists) {
+			this._selectedOptionsLists[type] = [];
+
+			this._optionsLists[type].forEach((option) => {
+				if (option.disabled) {
+					const optionDOM = document.querySelector(`.filters__option__list__item[data-value="${option.name}"]`);
+					optionDOM.classList.add("disabled");
+				} else {
+					const optionDOM = document.querySelector(`.filters__option__list__item[data-value="${option.name}"]`);
+					optionDOM.classList.remove("disabled");
+				}
+
+				if (option.selected) {
+					this._selectedOptionsLists[type].push(option.name);
+					this._selectedOptionsLists[type] = [...new Set(this._selectedOptionsLists[type])];
+				}
+			});
+
+			if (this._selectedOptionsLists[type].length == 0) {
+				this._selectedOptionsListsElements[type].classList.remove("show");
+			}
+		}
+
+		this.refreshSelectedOptions();
+	}
+
+	refreshSelectedOptions() {
+		for (let type in this._selectedOptionsListsElements) {
+			this._selectedOptionsListsElements[type].innerHTML = "";
+		}
+
+		for (let type in this._selectedOptionsLists) {
+			if (this._selectedOptionsLists[type].length > 0) {
+				this._selectedOptionsLists[type].forEach((selectedOption) => {
+					let li = createBlock("li", [
+						{ name: "class", value: "selected-filters__list__item" },
+						{ name: "data-value", value: selectedOption },
+						{ name: "data-type", value: type },
+					]);
+					let img = createImage("./assets/icons/close.svg", [{ name: "class", value: "selected-filters__list__close" }]);
+					let span = createBlock("span");
+
+					span.textContent = selectedOption;
+					li.append(span, img);
+
+					this._selectedOptionsListsElements[type].classList.add("show");
+					this._selectedOptionsListsElements[type].append(li);
+				});
+			}
+		}
+
+		this.initCloseOption();
+	}
+
+	initCloseOption() {
+		const items = document.querySelectorAll(".selected-filters__list__item");
+
+		items.forEach((item) => {
+			const close = item.querySelector(".selected-filters__list__close");
+
+			close.addEventListener("click", () => {
+				let clickedItem = this._optionsLists[item.dataset.type].filter((option) => clear(item.dataset.value) === clear(option.name));
+				this._optionsLists[item.dataset.type].map(() => {
+					clickedItem[0].selected = false;
+					clickedItem[0].disabled = false;
+				});
+
+				this.refreshOptionsDisplay();
+			});
+		});
+	}
+
+	initSearchBar() {
+		const searchBar = document.querySelector("#searchBar");
+		const MIN_INPUT_LENGTH = 3;
+
+		searchBar.addEventListener("input", (e) => {
+			let inputValue = e.target.value;
+
+			this._Recipes.forEach((recipe) => {
+				const card = document.querySelector("#recipe_" + recipe.id);
+
+				if (inputValue.length >= MIN_INPUT_LENGTH && recipeFilter(recipe, inputValue)) {
+					this.showElement(card);
+				} else if (inputValue.length < MIN_INPUT_LENGTH) {
+					this.showElement(card);
+				} else {
+					this.hideElement(card);
+				}
+			});
+		});
+
+		function recipeFilter(recipe, inputValue) {
+			return recipe.nameContains(inputValue) || recipe.descriptionContains(inputValue) || recipe.hasIngredient(inputValue);
+		}
 	}
 
 	initFiltersInputs() {
@@ -79,110 +209,6 @@ class App {
 				});
 			});
 		});
-
-		this.appendOptionsToLists();
-	}
-
-	appendOptionsToLists() {
-		for (let type in this._optionsListsElements) {
-			this._optionsLists[type].forEach((option) => {
-				let li = createBlock("li", [
-					{ name: "class", value: "filters__option__list__item" },
-					{ name: "data-value", value: option.name },
-					{ name: "data-type", value: type },
-				]);
-				li.textContent = option.name;
-
-				this._optionsListsElements[type].append(li);
-			});
-		}
-
-		this.initEventsToOptions();
-	}
-
-	initEventsToOptions() {
-		this._optionsElements = document.querySelectorAll(".filters__option__list__item");
-
-		this._optionsElements.forEach((option) => {
-			option.addEventListener("click", () => {
-				let clickedItem = this._optionsLists[option.dataset.type].filter((item) => clear(item.name) === clear(option.dataset.value));
-				this._optionsLists[option.dataset.type].map(() => {
-					clickedItem[0].selected = true;
-					clickedItem[0].disabled = true;
-				});
-
-				this.refreshOptionsDisplay();
-			});
-		});
-	}
-
-	refreshOptionsDisplay() {
-		for (let type in this._optionsLists) {
-			this._optionsLists[type].forEach((option) => {
-				if (option.disabled) {
-					const optionDOM = document.querySelector(`li[data-value="${option.name}"]`);
-					optionDOM.classList.add("disabled");
-				}
-
-				if (option.selected) {
-					this._selectedOptions[type].push(option);
-					this._selectedOptions[type] = [...new Map(this._selectedOptions[type].map((item) => [item.name, item])).values()];
-				}
-			});
-		}
-
-		this.refreshSelectedOptions();
-	}
-
-	refreshSelectedOptions() {
-		for (let type in this._selectedOptionsListsElements) {
-			this._selectedOptionsListsElements[type].innerHTML = "";
-		}
-
-		for (let type in this._selectedOptions) {
-			if (this._selectedOptions[type].length > 0) {
-				this._selectedOptions[type].forEach((selectedOption) => {
-					let li = createBlock("li", [
-						{ name: "class", value: "selected-filters__list__item" },
-						{ name: "data-value", value: selectedOption.name },
-						{ name: "data-type", value: type },
-					]);
-					let img = createImage("./assets/icons/close.svg", [{ name: "class", value: "selected-filters__list__close" }]);
-					let span = createBlock("span");
-
-					span.textContent = selectedOption.name;
-					li.append(span, img);
-
-					this._selectedOptionsListsElements[type].classList.add("show");
-					this._selectedOptionsListsElements[type].append(li);
-				});
-			}
-		}
-	}
-
-	initSearchBar() {
-		const searchBar = document.querySelector("#searchBar");
-		const MIN_INPUT_LENGTH = 3;
-
-		searchBar.addEventListener("input", (e) => {
-			let inputValue = e.target.value;
-
-			this._Recipes.forEach((recipe) => {
-				const card = document.querySelector("#recipe_" + recipe.id);
-
-				if (inputValue.length >= MIN_INPUT_LENGTH && recipeFilter(recipe, inputValue)) {
-					this.showElement(card);
-				} else if (inputValue.length < MIN_INPUT_LENGTH) {
-					this.showElement(card);
-				} else {
-					this.hideElement(card);
-				}
-			});
-		});
-
-		function recipeFilter(recipe, inputValue) {
-			return recipe.nameContains(inputValue) || recipe.descriptionContains(inputValue) || recipe.hasIngredient(inputValue);
-		}
 	}
 
 	showElement(element) {
